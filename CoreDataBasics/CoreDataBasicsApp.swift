7132ea736500258d78ad9b1d794be04f38385179
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-09-20 11:18:01 +0600

    project iniy

---
+++ CoreDataBasics/CoreDataBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataBasicsApp.swift
+//  CoreDataBasics
+//
+//  Created by Shah Md Imran Hossain on 20/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
