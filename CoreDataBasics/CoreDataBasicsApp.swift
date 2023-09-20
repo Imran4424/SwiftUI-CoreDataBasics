97afe8a05295473deebea465f187fb7c2bbf0e1e
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-09-20 11:41:03 +0600

    add as environment object

--- CoreDataBasics/CoreDataBasicsApp.swift
+++ CoreDataBasics/CoreDataBasicsApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct CoreDataBasicsApp: App {
+    @State private var dataController = DataController()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+            // managedObjectContext - live version of data
         }
     }
 }
